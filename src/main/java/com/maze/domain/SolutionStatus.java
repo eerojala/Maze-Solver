2b5d971e5f3b8d60d97379933f41112af9397617
eerojala <eero.ojala@helsinki.fi>
2023-09-12 21:20:42 +0300

    Add missing enum SolutionStatus to git, update .gitignore

---
+++ src/main/java/com/maze/domain/SolutionStatus.java
@@ -0,0 +1,8 @@
+package com.maze.domain;
+
+/**
+ * Enum for representing if the maze solving algorith resulted in a success, a failure or an error.
+ */
+public enum SolutionStatus {
+    SUCCESS, FAILURE, ERROR
+}
